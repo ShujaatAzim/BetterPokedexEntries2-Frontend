f16bc41c08d70ea62b7918ed144f1e0ba25b1706
Shujaat Azim <shujaat.azim@gmail.com>
2020-04-24 23:53:20 -0400

    Created login page component but didn't build it out.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import LoginPage from './Pages/LoginPage'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <Router>
     <React.StrictMode>
-      <Route exact path="/" component={App} />  
+      <Route exact path="/login" component={LoginPage} />
+      <Route exact path="/home" component={App} /> 
     </React.StrictMode>
   </Router>,
   document.getElementById('root')
