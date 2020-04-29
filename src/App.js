cfdd053bd6d76f4ce3b4f66079bb5fa270e819cf
Shujaat Azim <shujaat.azim@gmail.com>
2020-04-28 22:27:36 -0400

    Added presentational components to get the site looking as it did before. Might be able to just go back to the other one now that the backend is more set up.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import PokemonCardsContainer from './ContainerComponents/PokemonCardsContainer'
+import Jumbo from './Components/Jumbo'
+import NavBar from './Components/NavBar'
 
 const App = () => {
   return (
     <div>
+      <NavBar />
+      <Jumbo />
       <PokemonCardsContainer />
     </div>
   )
