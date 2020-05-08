07061da9ec77a5c3ca50aadcc67b4a64f7795b9f
Shujaat Azim <shujaat.azim@gmail.com>
2020-05-07 22:46:38 -0400

    Added login page stuff for registrations, and also modified the css file using floats.

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -1,25 +1,54 @@
 import React from 'react'
+import NavBar from '../Components/NavBar'
+import Jumbo from '../Components/Jumbo'
 import { Form, Button } from 'react-bootstrap'
+import '../Styles/App.css'
 
 const LoginPage = () => {
   return (
     <div>
-      <Form>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
-        <Form.Group controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
+      <NavBar />
+      <Jumbo />
+      <div className="app">
+        <div className="login">
+          <h3>Login!</h3>
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </div>
+        <div className="register">
+          <h3>Or register!</h3>
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </div>
+      </div>
     </div>
   )
 }
