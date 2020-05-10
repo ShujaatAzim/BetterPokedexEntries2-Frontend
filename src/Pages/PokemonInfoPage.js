b882a8360b60c023b2e7eef24487962b8a057a61
Shujaat Azim <shujaat.azim@gmail.com>
2020-05-10 00:00:01 -0400

    Added logic for profile page of pokemon.

--- src/Pages/PokemonInfoPage.js
+++ src/Pages/PokemonInfoPage.js
@@ -3,9 +3,19 @@ import React from 'react'
 const PokemonInfoPage = props => {
   return (
     <div>
-      <h1>
-        {props.pokemon.name}
-      </h1>
+      <div style={{ textAlign: "center" }}>
+        <h1>{pokemon.name}</h1>
+        <br />
+        <img src={`https://img.pokemondb.net/artwork/${pokemon.name.toLowerCase()}.jpg`} alt={`${pokemon.name}`} />
+        <br />
+        <h3>{pokemon.species} Pokemon</h3>
+        <br />
+        <h5>{pokemon.pokedex}</h5>
+        <br />
+        <Button style={{ margin: "1rem" }}>Edit</Button>
+        <Button onClick={() => setComplete(!complete)} variant={complete ? "success" : "outline-secondary"} style={{ marginLeft: "0.5rem" }}>
+              { complete? "Complete!" : "Complete?" }</Button>
+      </div>
     </div>
   )
 }
