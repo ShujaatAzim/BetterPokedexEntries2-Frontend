314fabae7d1dad6245d73f2cdc089f61b3bc57b6
Shujaat Azim <shujaat.azim@gmail.com>
2020-05-11 22:29:02 -0400

    Built out architecture for the All Pokemon page list. Will eventually have filters in the navbar so that it can be filtered based on region.

--- src/Pages/PokemonPage.js
+++ src/Pages/PokemonPage.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import NavBar from '../Components/NavBar'
+import Jumbo from '../Components/Jumbo'
 
 const PokemonPage = () => {
   return (
     <div>
+      <NavBar />
+      <Jumbo />
       <h1>All Pokemon</h1>
+      <PokemonCardsContainer />
     </div>
   )
 }
